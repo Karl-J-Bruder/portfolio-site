aa9fa8d61504b86133a7cde6c29d2c4a7cc581f4
Karl Bruder <karl.j.bruder@gmail.com>
2020-06-26 12:11:18 +0900

    Added new contact details

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,7 +14,8 @@ const ContactPage = () => {
                     <ul>
                         <li>Email: karl.j.bruder@gmail.com</li>
                         <li>GitHub: <a href="https://github.com/Karl-J-Bruder" target="_blank" rel="noreferrer">Karl-J-Bruder</a></li>
-                        <li>LinkedIn: @@@@@</li>
+                        <li>LinkedIn: <a href="https://www.linkedin.com/in/karl-bruder-a0b1a11a9/" target="_blank" rel="noreferrer">Karl Bruder</a></li>
+                        <li>Wantedly（日本語）: <a href="https://www.wantedly.com/users/138073753" target="_blank" rel="noreferrer">ブルーダー・カール</a></li>
                     </ul>
                     <p>Thank you for your interest, and I hope to hear from you soon!</p>
                 </div>
